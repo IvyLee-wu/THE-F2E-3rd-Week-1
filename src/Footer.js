fa95469653dadaba6c6a221dfd862c494760eacc
IvyLee-wu <iiivvvyyy01@gmail.com>
2021-11-07 11:30:16 +0800

    update

--- src/Footer.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-const Footer = () => {
-  return (
-    <div className="footer">
-      <span>THE F2E 3rd Week 01 Desin by : Frank Peng</span>
-    </div>
-  );
-};
-export default Footer;
